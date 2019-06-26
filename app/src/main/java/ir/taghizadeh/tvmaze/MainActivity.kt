3e6001d59ee4865b84b99005f0fb89f43adbf138
a.taghizadeh <a.taghizadeh@netbarg.com>
2019-06-26 15:49:04 +0430

    Add needed dependencies

--- app/src/main/java/ir/taghizadeh/tvmaze/MainActivity.kt
+++ app/src/main/java/ir/taghizadeh/tvmaze/MainActivity.kt
@@ -1,7 +1,7 @@
 package ir.taghizadeh.tvmaze
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
