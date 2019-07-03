613dd100524eb0775a9cc60d3a9e1733275d7545
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2019-07-03 23:14:29 +0430

    implement getAlbumService method

---
+++ app/src/main/java/ir/taghizadeh/deezer/data/network/services/ChartAlbumsService.kt
@@ -0,0 +1,12 @@
+package ir.taghizadeh.deezer.data.network.services
+
+import ir.taghizadeh.deezer.data.network.responses.ChartTracksResponse
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+
+//https://api.deezer.com/chart/0/albums
+
+interface ChartAlbumsService {
+    @GET("chart/0/albums")
+    fun getChartAlbums(): Deferred<ChartTracksResponse>
+}
--- app/src/main/java/ir/taghizadeh/deezer/view/albumList/AlbumListFragment.kt
+++ app/src/main/java/ir/taghizadeh/deezer/view/albumList/AlbumListFragment.kt
@@ -1,13 +1,19 @@
 package ir.taghizadeh.deezer.view.albumList
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ir.taghizadeh.deezer.R
+import ir.taghizadeh.deezer.data.network.ApiClient
+import ir.taghizadeh.deezer.data.network.services.ChartAlbumsService
 import kotlinx.android.synthetic.main.fragment_album_list.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AlbumListFragment : Fragment() {
 
@@ -20,6 +26,11 @@ class AlbumListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleNavigation()
+        val apiService = ApiClient.buildService(ChartAlbumsService::class.java)
+        GlobalScope.launch(Dispatchers.Main) {
+            val chartAlbums = apiService.getChartAlbums().await()
+            Log.d("ChartAlbumsResponse: ", chartAlbums.data.toString())
+        }
     }
 
     private fun handleNavigation() {
